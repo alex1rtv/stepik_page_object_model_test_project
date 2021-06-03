520c2749ed2e2efd03585b9d7b175449cae69609
alex1rtv <alex1rtv@gmail.com>
2021-06-03 16:20:54 +0300

    put link back

--- test_main_page.py
+++ test_main_page.py
@@ -30,6 +30,8 @@ from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+ 
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
@@ -40,6 +42,7 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
@@ -48,8 +51,7 @@ def test_guest_should_see_login_link(browser):
 # https://stepik.org/lesson/238819/step/6?unit=211271
 
 def should_be_login_link(self):
-    assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), \
-           "Login link is not presented"
+    assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
     
     
 
