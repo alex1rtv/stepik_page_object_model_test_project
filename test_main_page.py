8ce8463057af3ea37aa07ddf6cfebf44dabc8e01
alex1rtv <alex1rtv@gmail.com>
2021-06-03 15:37:51 +0300

    update exception message

--- test_main_page.py
+++ test_main_page.py
@@ -35,6 +35,25 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 
+# 4.2.5
+# https://stepik.org/lesson/238819/step/5?unit=211271
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+# 4.2.6
+# https://stepik.org/lesson/238819/step/6?unit=211271
+
+def should_be_login_link(self):
+    assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), \
+           "Login link is not presented"
+    
+    
+
+
 
 
 
